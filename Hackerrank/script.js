9fa4b010ee3b451a1d4ea4f75e9dc4dd899d34aa
Vinicius Gabriel Viana <85412817+viniciusgabrielviana@users.noreply.github.com>
2023-10-09 12:00:10 -0300

    novos exercicios do site hackerrank para treino

---
+++ Hackerrank/script.js
@@ -0,0 +1,42 @@
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+function simpleArraySum(ar) {
+    let soma = 0
+    for (let i = 0; i < ar.length; i++) {
+        soma = soma + ar[i]
+        return soma
+    }
+
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const arCount = parseInt(readLine().trim(), 10);
+
+    const ar = readLine().replace(/\s+$/g, '').split(' ').map(arTemp => parseInt(arTemp, 10));
+
+    const result = simpleArraySum(ar);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
